832bbc10a59fe43c69ea0d5823e6573192171f8f
Venancio Angelo Belga <venancio.belga@autoglass.com.br>
2023-10-18 12:24:16 -0300

    Parão Factory Method

---
+++ DesingPatterns/FactoryMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using FactoryMethod.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transport transport = null;
+
+            if (args.Length > 0 && args[0] == "--uber")
+            {
+                transport = new CarTransport(); 
+            }
+            else if (args.Length > 0 && args[0] == "--log")
+            {
+                transport = new MotoCycleTransport();
+            }
+            else if (args.Length > 0 && args[0] == "--bike")
+            {
+                transport = new BicycleTransport();
+            }
+            else
+            {
+                Console.WriteLine("Selecione o tipo de serviço");
+            }
+
+            if (transport != null)
+            {
+                transport.StartTransport();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesingPatterns/FactoryMethod/Vehicles/Bicycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.Vehicles
+{
+    class Bicycle: IVehicle
+    {
+        public void GetCargo()
+        {
+            Console.WriteLine("Pegamos a entrega");
+        }
+
+        public void StartRoute()
+        {
+            GetCargo();
+            Console.WriteLine("Iniciamos a corrida");
+        }
+    }
+}
